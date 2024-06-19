9ce10be98c7900af49f4db26ab6a48e11993d3e9
alansemenov <ase@enonic.com>
2024-06-19 13:43:26 +0200

    Upgraded gradle. Fixed asset url. Added build workflow.

--- src/main/resources/webapp/webapp.js
+++ src/main/resources/webapp/webapp.js
@@ -1,15 +1,17 @@
+const portalLib = require('/lib/xp/portal');
+
 exports.get = function (req) {
-  var title = 'Hello Web app';
+  const title = 'Hello Web app';
   return  {
   body: `
 <html>
   <head>
     <title>${title}</title>
-    <link rel="stylesheet" type="text/css" href="styles.css"/>
+    <link rel="stylesheet" type="text/css" href="${portalLib.assetUrl({ path: 'styles.css' })}"/>
   </head>
   <body>
       <h1>Sweet, "${title}" is working!</h1>
-      <img src="html5logo.svg"/>
+      <img src="${portalLib.assetUrl({ path: 'html5logo.svg' })}" alt=""/>
   </body>
 </html>
 `
